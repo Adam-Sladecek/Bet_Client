9aa6b8d1c43bc233e2b4df6094e8adf23051973b
Adam Sládeček <adamsladecek0@gmail.com>
2025-03-01 13:39:46 +0100

    fixed ports

--- src/app/constants/app.constants.ts
+++ src/app/constants/app.constants.ts
@@ -1,9 +1,9 @@
 export const API_CONSTANTS = {
-    BASE_URL: 'http://127.0.0.1:8000/api/',
+    BASE_URL: 'http://localhost:80/api/',
 };
 
 export const SOCKET_CONSTANTS = {
-    URL: 'ws://localhost:8000/ws/scrape/',
+    URL: 'ws://localhost:80/ws/scrape/',
     START: 'start',
     END: 'end',
     IMPORT: 'import',
